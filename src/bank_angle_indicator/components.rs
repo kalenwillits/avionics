3aa5d1a8405fe86d7ea05c2181b714da4d26cb47
Kalen <kalenwillits@gmail.com>
2023-11-13 21:25:33 -0500

    bank angle needle

--- src/bank_angle_indicator/components.rs
+++ src/bank_angle_indicator/components.rs
@@ -5,3 +5,6 @@ pub struct BankAngleIndicator;
 
 #[derive(Component)]
 pub struct BankAngleLine;
+
+#[derive(Component)]
+pub struct BankAngleNeedle;
--- src/bank_angle_indicator/systems.rs
+++ src/bank_angle_indicator/systems.rs
@@ -1,4 +1,4 @@
-use super::components::{BankAngleIndicator, BankAngleLines};
+use super::components::{BankAngleIndicator, BankAngleLine, BankAngleNeedle};
 use crate::utils::degrees_to_radians;
 use crate::xplane_listener::AircraftState;
 use bevy::prelude::*;
@@ -26,6 +26,7 @@ pub fn spawn_bank_angle_indicator(mut commands: Commands) {
                     scale: (1.6, 1.7, 1.0).into(),
                     ..default()
                 },
+                z_index: ZIndex::Local(10),
                 ..default()
             },
             Name::new("BankAngleIndicator"),
@@ -33,33 +34,52 @@ pub fn spawn_bank_angle_indicator(mut commands: Commands) {
         ))
         .with_children(|parent| {
             parent
-                .spawn(NodeBundle {
-                    style: Style {
-                        width: Val::Px(BANK_ANGLE_TICK_WIDTH),
-                        height: Val::Percent(BANK_ANGLE_RADIUS - BANK_ANGLE_TICK_SIZE),
-                        flex_direction: FlexDirection::Column,
-                        align_items: AlignItems::Start,
-                        justify_content: JustifyContent::Start,
-                        ..default()
-                    },
-                    ..default()
-                })
-                .with_children(|parent| {
-                    parent.spawn(NodeBundle {
+                .spawn((
+                    BankAngleNeedle {},
+                    NodeBundle {
                         style: Style {
-                            height: Val::Percent(BANK_ANGLE_TICK_SIZE * 2.0),
                             width: Val::Percent(100.0),
+                            height: Val::Percent(100.0),
+                            flex_direction: FlexDirection::Column,
+                            align_items: AlignItems::Center,
+                            justify_content: JustifyContent::Center,
+                            position_type: PositionType::Absolute,
                             ..default()
                         },
-                        background_color: Color::WHITE.into(),
+                        z_index: ZIndex::Local(10),
                         ..default()
-                    });
+                    },
+                ))
+                .with_children(|parent| {
+                    parent
+                        .spawn(NodeBundle {
+                            style: Style {
+                                width: Val::Px(BANK_ANGLE_TICK_WIDTH),
+                                height: Val::Percent(BANK_ANGLE_RADIUS - BANK_ANGLE_TICK_SIZE),
+                                flex_direction: FlexDirection::Column,
+                                align_items: AlignItems::Start,
+                                justify_content: JustifyContent::Start,
+                                ..default()
+                            },
+                            ..default()
+                        })
+                        .with_children(|parent| {
+                            parent.spawn(NodeBundle {
+                                style: Style {
+                                    height: Val::Percent(BANK_ANGLE_TICK_SIZE * 2.0),
+                                    width: Val::Percent(100.0),
+                                    ..default()
+                                },
+                                background_color: Color::WHITE.into(),
+                                ..default()
+                            });
+                        });
                 });
 
             for degree in BANK_ANGLE_DEGREES.iter() {
                 parent
                     .spawn((
-                        BankAngleLines {},
+                        BankAngleLine {},
                         NodeBundle {
                             style: Style {
                                 width: Val::Percent(100.0),
@@ -124,7 +144,7 @@ pub fn spawn_bank_angle_indicator(mut commands: Commands) {
 
 pub fn update_bank_angle_lines(
     aircraft_state: Res<AircraftState>,
-    mut pitch_lines_queryset: Query<&mut Transform, With<BankAngleLines>>,
+    mut pitch_lines_queryset: Query<&mut Transform, With<BankAngleLine>>,
 ) {
     for mut pitch_line_transform in pitch_lines_queryset.iter_mut() {
         pitch_line_transform.rotation.z = degrees_to_radians(aircraft_state.roll) * -0.5;
